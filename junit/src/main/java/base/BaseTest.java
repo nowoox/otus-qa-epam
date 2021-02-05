22417f2d0631a0f99ce8230b7c023a196446ef79
nowoox <nowoox@gmail.com>
2021-02-05 18:42:51 +0300

    Docker test v.5

--- junit/src/main/java/base/BaseTest.java
+++ junit/src/main/java/base/BaseTest.java
@@ -59,7 +59,7 @@ public class BaseTest {
         options.addArguments("--disable-gpu"); // applicable to windows os only
         options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
         options.addArguments("--no-sandbox"); // Bypass OS security model
-        options.addArguments("--whitelisted-ips=''");
+        options.addArguments("--whitelisted-ips='127.0.0.1'");
         WebDriver driver = new RemoteWebDriver(options);
 
 
